8f0ef12880af7b8810f695dc9f1b0de712f08642
smali <smali@DESKTOP-FQ02UPM>
2016-12-31 16:45:41 +0100

    Core app

---
+++ HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class GreetPage : ContentPage
+    {
+
+        public GreetPage()
+        {
+            InitializeComponent();
+        }
+        void Handle_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Title","Hello World","ok");
+        }
+    }
+}
